3401e3e1dc7ea2a1595f60f7663f040a683a800e
Kenan Curevac <kenancurevac@gmail.com>
2025-02-15 22:05:49 +0100

    final update before ts

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,12 +1,29 @@
 import "./Review.css";
+import { useRef } from "react";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 export default function Review({ history, myPicks }) {
+  const accordionRef = useRef(null);
+
+  function handleAccordionExpansion(event, isExpanded) {
+    if (accordionRef.current && isExpanded) {
+      setTimeout(() => {
+        accordionRef.current.scrollIntoView({
+          behavior: "smooth",
+        });
+      }, 200);
+    }
+  }
+
   return (
-    <Accordion square={false}>
+    <Accordion
+      square={false}
+      ref={accordionRef}
+      onChange={handleAccordionExpansion}
+    >
       <AccordionSummary
         expandIcon={<ExpandMoreIcon />}
         aria-controls="panel2-content"
